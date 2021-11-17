6f5bccef9af3c4f23db48bead8e11e29c6256d9d
Gregory Newman <greg@naughton.org>
2021-11-16 20:11:42 -0700

    still roughing it in

--- Project Part 3/main.py
+++ Project Part 3/main.py
@@ -1,4 +1,8 @@
 
+from sql import sqlWrapper
+from dialogues import Question, ynQuestion, SubMenu
+import sys
+
 def DisplayDigitalDisplays():
     """
         (1) Display all the digital displays. For each display, if a user clicks the model no, the detailed model information should be displayed.
@@ -41,11 +45,26 @@ def login_dialog():
     Once users input these information, the
     database should show whether it has connected to the database correctly.
     """
-    print('Connected Successfully!')
+
+    while True:
+        opt = SubMenu(['login'], 'Please login to begin', exit=False)
+        if opt is None:
+            db = False
+            return db
+
+        if opt == 'login':
+            # host, database, username, password
+            host = Question( 'To connect, please enter the host > ')
+            database = Question( 'To connect, please enter the database > ')
+            username = Question( 'To connect, please enter the username> ')
+            password = Question( 'To connect, please enter the password> ')
 
 
+            db = sqlWrapper(host, database, username, password)
+            return db
 
-def main_menu():
+
+def main_menu(db):
     """
     - 1. Display all the digital displays.
     - 2. Search digital displays given a scheduler system
@@ -54,7 +73,16 @@ def main_menu():
     - 5. Update a digital display
     - 6. Logout
     """
-    pass
+    menuopts = [ "Display all the digital displays.",
+                 "Search digital displays given a scheduler system",
+                 "Insert a new digital display",
+                 "Delete a digital display",
+                 "Update a digital display",
+                 "Logout" ]
+    while True:
+        opt = SubMenu(menuopts, "Please select an option", exit=False)
+        if opt is None:
+            break
 
 
 if __name__ == '__main__':
@@ -62,14 +90,21 @@ if __name__ == '__main__':
     prompt users to input database host,
     database name, username and password.
     """
-    login_dialog()
+    db = login_dialog()
+    if db:
+        if db.is_connected():
+            print('Database has connected correctly')
+        else:
+            print('Database failed to connect', file=sys.stderr)
+            sys.exit(123)
+    else:
+        sys.exit(0)
 
     """
     It should list
     options to conduct the Main functions (see below) and to logout. One example of an
     interface can be a list as follows
     """
-    main_menu()
-
+    main_menu(db)
 
 
---
+++ Project Part 3/sql.py
@@ -0,0 +1,40 @@
+import mysql.connector
+import sys
+
+class sqlWrapper:
+    def __init__(self, h, d, u, p):
+        self.db = mysql.connector.connect(
+            host = h,
+            user = u,
+            password = p
+        )
+        self.host = h
+        self.database = d
+        self.username = u
+        self.password = p
+        if not self.db.is_connected():
+            sys.exit(1)
+        # https://stackoverflow.com/questions/38350816/python-mysql-connector-internalerror-unread-result-found-when-close-cursor
+        self.cursor = self.db.cursor(buffered=True)
+        self.cursor.execute('use {}'.format(self.database))
+
+
+    def is_connected():
+        return self.db.is_connected()
+
+
+    def query(self, qstring):
+        self.cursor.execute(qstring)
+        return [x[0] if len(x) == 1 else x for x in self.cursor]
+
+
+    def close(self):
+        if self.db.is_connected():
+            self.cursor.close()
+            self.db.close()
+            assert(not self.db.is_connected())
+            return True
+        else:
+            return False
+
+
